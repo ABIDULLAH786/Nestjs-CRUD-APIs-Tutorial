314e772339d3d5db8d9050a718a7e29574437e1c
abidullah786 <abidullah.se@gmail.com>
2023-08-28 10:59:56 +0500

    prisma setuped in project

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+// Route /auth/*
+@Controller('auth')
 
 export class AuthController{
+    constructor(private authService: AuthService) { }
+    
+    // Route: /auth/signup
+    @Post('signup')
+    signup() {
+        return this.authService.signup();
+    }
 
+    // Route: /auth/signin
+    @Post('signin')
+    signin() {
+        return this.authService.signin();
+    }
     
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
 
 
 @Injectable({})
 
-export class AuthService { }
+export class AuthService {
+    signup() {
+        return { status: 'success', statusCode: HttpStatus.CREATED, msg: "This is sign up api" }
+    }
+
+    signin() {
+        return { status: 'success', statusCode: HttpStatus.OK, msg: "This is sign in api" }
+    }
+}
