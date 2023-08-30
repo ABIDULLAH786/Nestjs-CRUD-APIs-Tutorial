218417ae1c5934f8d6c135b670a1eb7542b85c81
abidullah786 <abidullah.se@gmail.com>
2023-08-30 07:18:35 +0500

    custome devorator for getting single users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, HttpStatus, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+
+import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
@@ -7,8 +8,8 @@ export class UsersController {
     // Get: users/me
     @UseGuards(JwtGuard)  // protecting route
     @Get('/me')
-    getMe(@Req() req: Request) {
-        return { status: 'success', statusCode: HttpStatus.ACCEPTED, data: req.user }
+    getMe(@GetUser() user) {
+        return { status: 'success', statusCode: HttpStatus.ACCEPTED, data: user }
 
     }
 }
